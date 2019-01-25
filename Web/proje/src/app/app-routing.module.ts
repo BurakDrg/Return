ccbc3cb79eb76783a247fecfe670c2500f077175
utku <utku.cuhadaroglu@gmail.com>
2019-01-25 23:23:53 +0300

    Added Routing for easy merge

--- Web/proje/src/app/app-routing.module.ts
+++ Web/proje/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DenemeComponent} from './deneme/deneme.component';
 
-const routes: Routes = [];
+  /*Add This to Routes array for main page redirection: (utku)
+{ path: '', redirectTo: '/deneme', pathMatch: 'full' }
+
+*/
+const routes: Routes = [
+  {path: 'deneme', component: DenemeComponent}
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
