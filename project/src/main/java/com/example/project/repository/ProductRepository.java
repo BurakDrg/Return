39f9dd9aa8b1b61173ee8ebc9761c2bfa8d3698f
BurakDrg <burakdurgunsu@hotmail.com>
2019-01-27 19:22:29 +0300

    New Model added and android connected with backend

--- project/src/main/java/com/example/project/repository/ProductRepository.java
+++ project/src/main/java/com/example/project/repository/ProductRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import com.example.project.model.User;
+import com.example.project.model.Product;
 
 @SuppressWarnings("unused")
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-	List<User> findAllByUserid(long userid);
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+	List<Product> findAllByUserid(long id);
 }
