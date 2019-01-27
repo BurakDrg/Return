0279c4716a6e2ea29d0af356d59b378bdd93063c
Berk Bacalan <berkbacalan@gmail.com>
2019-01-27 20:17:38 +0300

    Fixed bugs and optimization.(Data Parsing Issues)

--- OrderListWeb/OrderList-angular/src/app/app.module.ts
+++ OrderListWeb/OrderList-angular/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes: Routes = [
+  {path: 'products', component: ProductComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'cart', component: CartComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ProductComponent,
+      CategoryComponent,
+      CartComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      RouterModule.forRoot(routes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
