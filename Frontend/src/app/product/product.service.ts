3bba6777bfa92047b9bc2b7c8d39eee2d0136698
Berk Bacalan <berkbacalan@gmail.com>
2019-03-23 14:01:10 +0300

    ready for merge

--- Frontend/src/app/product/product.service.ts
+++ Frontend/src/app/product/product.service.ts
@@ -24,12 +24,9 @@ export class ProductService {
     return this.http.get<Array<Product>>(this.baseURL);
   }
 
-  createProduct(product: Product) {
-    return this.http.post(this.baseURL, product);
+  public createProduct(product) {
+    return this.http.post<Product>(this.baseURL, product);
   }
-  // updateProduct(product: Product) {
-  //   return this.http.put(this.baseURL + '/' + product.id, product);
-  // }
   deleteProduct(id: number) {
     return this.http.delete(this.baseURL + '/' + id);
   }
